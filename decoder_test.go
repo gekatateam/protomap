483d6e86cf94bdc9ea904a9383e66b26514d0680
M0rdecay <relikts2158@gmail.com>
2025-07-30 20:28:46 +0300

    interceptors for custom encoding/decoding messages

--- decoder_test.go
+++ decoder_test.go
@@ -5,8 +5,11 @@ import (
 	"os"
 	"reflect"
 	"testing"
+	"time"
 
+	"github.com/bufbuild/protocompile"
 	"github.com/gekatateam/protomap"
+	"github.com/gekatateam/protomap/interceptors"
 )
 
 func TestDecoder_DecodeToJson(t *testing.T) {
@@ -37,6 +40,60 @@ func TestDecoder_DecodeToJson(t *testing.T) {
 	}
 
 	result, err := mapper.Decode(binary, testMessage)
+	if err != nil {
+		t.Fatalf("binary data decoding failed: %v", err)
+	}
+
+	if !reflect.DeepEqual(expected, result) {
+		t.Log("------ expected -----")
+		for k, v := range expected {
+			t.Logf("%v %v %T", k, v, v)
+		}
+		t.Log("------ result -----")
+		for k, v := range result.(map[string]any) {
+			t.Logf("%v %v %T", k, v, v)
+		}
+		t.Fatal("expected and result are not equal")
+	}
+}
+
+func TestDecoder_DecodeWithInters(t *testing.T) {
+	compiler := protocompile.Compiler{
+		Resolver: protocompile.WithStandardImports(&protocompile.SourceResolver{}),
+	}
+
+	mapper, err := protomap.NewMapper(&compiler, testIntersProto)
+	if err != nil {
+		t.Fatalf("decoder creation failed: %v", err)
+	}
+
+	binary, err := os.ReadFile(testIntersBinary)
+	if err != nil {
+		t.Fatalf("binary data reading failed: %v", err)
+	}
+
+	tjson, err := os.ReadFile(testIntersJson)
+	if err != nil {
+		t.Fatalf("json data reading failed: %v", err)
+	}
+
+	expected := make(map[string]any)
+	err = json.Unmarshal(tjson, &expected)
+	if err != nil {
+		t.Fatalf("json data unmarshaling failed: %v", err)
+	}
+
+	expected["Ts"], err = time.Parse(time.RFC3339, expected["Ts"].(string))
+	if err != nil {
+		t.Fatalf("json time unmarshaling failed: %v", err)
+	}
+
+	expected["Dur"], err = time.ParseDuration(expected["Dur"].(string))
+	if err != nil {
+		t.Fatalf("json duration unmarshaling failed: %v", err)
+	}
+
+	result, err := mapper.Decode(binary, testIntersMessage, interceptors.DurationDecoder, interceptors.TimeDecoder)
 	if err != nil {
 		t.Fatalf("binary data decoding failed: %v", err)
 	}
--- encoder_test.go
+++ encoder_test.go
@@ -5,8 +5,11 @@ import (
 	"os"
 	"reflect"
 	"testing"
+	"time"
 
+	"github.com/bufbuild/protocompile"
 	"github.com/gekatateam/protomap"
+	"github.com/gekatateam/protomap/interceptors"
 )
 
 func TestEncoder_EncodeToBinaryThenDecode(t *testing.T) {
@@ -70,6 +73,75 @@ func TestEncoder_EncodeToBinaryThenDecode(t *testing.T) {
 	if !reflect.DeepEqual(expected, decoderesult) {
 		t.Log("------ expected -----")
 		for k, v := range expected {
+			t.Logf("%v %v %T", k, v, v)
+		}
+		t.Log("------ result -----")
+		for k, v := range result {
+			t.Logf("%v %v %T", k, v, v)
+		}
+		t.Fatal("expected and result are not equal")
+	}
+}
+
+func TestEncoder_EncodeWithIntersThenDecode(t *testing.T) {
+	compiler := protocompile.Compiler{
+		Resolver: protocompile.WithStandardImports(&protocompile.SourceResolver{}),
+	}
+
+	mapper, err := protomap.NewMapper(&compiler, testIntersProto)
+	if err != nil {
+		t.Fatalf("decoder creation failed: %v", err)
+	}
+
+	binary, err := os.ReadFile(testIntersBinary)
+	if err != nil {
+		t.Fatalf("binary data reading failed: %v", err)
+	}
+
+	tjson, err := os.ReadFile(testIntersJson)
+	if err != nil {
+		t.Fatalf("json data reading failed: %v", err)
+	}
+
+	input := make(map[string]any)
+	err = json.Unmarshal(tjson, &input)
+	if err != nil {
+		t.Fatalf("json data unmarshaling failed: %v", err)
+	}
+
+	input["Ts"], err = time.Parse(time.RFC3339, input["Ts"].(string))
+	if err != nil {
+		t.Fatalf("json time unmarshaling failed: %v", err)
+	}
+
+	input["Dur"], err = time.ParseDuration(input["Dur"].(string))
+	if err != nil {
+		t.Fatalf("json duration unmarshaling failed: %v", err)
+	}
+
+	result, err := mapper.Encode(input, testIntersMessage, interceptors.DurationEncoder, interceptors.TimeEncoder)
+	if err != nil {
+		t.Fatalf("map input encoding failed: %v", err)
+	}
+
+	// we did this because fields order are not determined
+	// if !reflect.DeepEqual(binary, result) {
+	if len(binary) != len(result) {
+		t.Log("------ expected -----")
+		t.Logf("%v", binary)
+		t.Log("------ result -----")
+		t.Logf("%v", result)
+		t.Fatal("expected and result length are not equal")
+	}
+
+	decoderesult, err := mapper.Decode(binary, testIntersMessage, interceptors.DurationDecoder, interceptors.TimeDecoder)
+	if err != nil {
+		t.Fatalf("binary data decoding failed: %v", err)
+	}
+
+	if !reflect.DeepEqual(input, decoderesult) {
+		t.Log("------ expected -----")
+		for k, v := range input {
 			t.Logf("%v %v %T", k, v, v)
 		}
 		t.Log("------ result -----")
